56bfa333eef0ca349210a9e3c0d9f6748667612b
mrawan937 <mrawan12309@gmail.com>
2025-05-09 06:41:09 +0300

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('bookingForm').addEventListener('submit', function(e) {
-  e.preventDefault();
-  document.getElementById('bookingForm').classList.add('hidden');
-  document.getElementById('confirmation').classList.remove('hidden');
-});
